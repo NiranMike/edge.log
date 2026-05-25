5764ee38c69570fd0efacef50443441207da52d4
NiranMike <olaniranmicheal2020@gmail.com>
2026-05-25 14:45:12 +0100

    update

---
+++ prisma/migrations/20260525134401_remove_pnl_from_trades/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `pnl` on the `trades` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "trades" DROP COLUMN "pnl";
